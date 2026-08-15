92c1fddac58041eda0f83ec455b204c5ae424287
Dmitry Petrov <dimakrm361@gmail.com>
2026-08-15 22:39:43 +0900

    chore: wire packages/sdk and mods/necesse into root build/test pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "legacy", "node_modules", "src/components/ui"] },
+  { ignores: ["**/dist/**", "legacy", "node_modules", "src/components/ui"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
